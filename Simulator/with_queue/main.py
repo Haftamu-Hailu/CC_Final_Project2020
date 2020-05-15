e904e58d52d1eba7de4cec714babf87ec6e0c15c
Jorrit <palfner.jorrit@est.fib.upc.edu>
2020-05-15 09:37:27 +0200

    queue added

---
+++ Simulator/with_queue/main.py
@@ -0,0 +1,99 @@
+import boto3
+import time
+import json
+from initializer import initialize
+from simulator import Simulator
+from saver import Saver
+
+def start_simulation(initial_data, verbose=True):
+    enable_contact_tracing = initial_data['contact_tracing']  # Enable contact tracing
+    total_agents = initial_data['total_agents']  # Number of Agents
+    initially_infected_agents = initial_data['infected_agents']  # Number of initially infected agents
+    initially_healthy_agents = total_agents - initially_infected_agents  # Number of initially healthy agents
+    office_capacity = initial_data['office_capacity']  # Capacity of agents per office
+    house_capacity = initial_data['home_capacity']  # Capacity of agents per house
+    mortality_rate = initial_data['mortality_rate']  # Mortality rate
+    total_days_sick = initial_data['sick_days']  # Number of days sick
+    days_until_symptoms = initial_data['free_symptoms_days']  # Number of days until symptoms
+    total_days_simulated = initial_data['total_days']  # Number of days of simulation
+    risk_infection_home = initial_data['risk_home']  # Risk of infection at home
+    risk_infection_work = initial_data['risk_work']  # Risk of infection at work
+    verbose = verbose  # If we want printing during simulator run
+
+    locations, agent_array = initialize(total_agents, initially_infected_agents, initially_healthy_agents,
+                                        office_capacity, house_capacity, mortality_rate, total_days_sick,
+                                        days_until_symptoms, total_days_simulated, risk_infection_home,
+                                        risk_infection_work)
+
+    saver = Saver(verbose)
+    simulator = Simulator(enable_contact_tracing, locations, agent_array, saver)
+
+    while simulator.current_day <= total_days_simulated:
+        simulator.step()
+
+    return 200, saver.overview
+
+
+def send_message(message):
+    sqs = boto3.client('sqs',region_name='eu-west-2',
+                    aws_access_key_id="AKIAWWEQQOG3FWVNHG4V", 
+                    aws_secret_access_key="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")
+    queue_url = 'https://sqs.eu-west-2.amazonaws.com/459864568246/conf_queue'
+    sqs.send_message(
+    QueueUrl=queue_url,
+    DelaySeconds=10,
+    MessageAttributes={
+        'Email': {
+            'DataType': 'String',
+            'StringValue': 'Simulation Start successful'
+        },
+        },
+    MessageBody= message
+    )
+    print("sent message")
+
+def main():
+    sqs = boto3.client('sqs',region_name='eu-west-2',
+                    aws_access_key_id="AKIAWWEQQOG3FWVNHG4V", 
+                    aws_secret_access_key="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")
+    
+    #ditc = json.dumps({"initial_data" : {"contact_tracing" : 0, "total_agents": 10, "infected_agents": 1, "office_capacity" : 5, "home_capacity" : 2, "mortality_rate":0.04, "sick_days": 21, "free_symptoms_days":14 ,"total_days":30, "risk_home": 0.1, "risk_work": 0.03}})
+    #send_message(ditc)
+
+    while 1:
+        time.sleep(1)
+        queue_url = 'https://sqs.eu-west-2.amazonaws.com/459864568246/sim_test'
+        response = sqs.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=30,
+        WaitTimeSeconds=0
+        )
+
+        #print(response)
+        try:
+            receipt_handle = response['Messages'][0]['ReceiptHandle']
+            sqs.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=receipt_handle
+            )
+            #print(response['Messages'][0]['Body'])
+            argv = json.loads(response['Messages'][0]['Body'])
+            #print(argv)
+            #print(argv)
+            #for i in argv['initial_data']:
+            #        print(argv['initial_data'][i])
+            start_simulation(argv['initial_data'])
+            send_message(argv)
+            #continue
+        except Exception as e:
+            print("Queue is empty")
+
+if __name__ == '__main__':
+    main()
